18647159dad08dbc39d661ced21219fb566b6fba
Eder Fan <fanedercode@gmail.com>
2019-05-01 00:38:42 +0100

    Updates

---
+++ precache-manifest.5a6f6539937428889d113a7266d6bf5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c774bcea9b6aa65f9a34973623ae26d5",
+    "url": "/react-draggable-hooks/index.html"
+  },
+  {
+    "revision": "b820e323406bc518a020",
+    "url": "/react-draggable-hooks/static/css/main.9bf50cb9.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react-draggable-hooks/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "b820e323406bc518a020",
+    "url": "/react-draggable-hooks/static/js/main.2885e336.chunk.js"
+  },
+  {
+    "revision": "8b6f8aa56e42c9533ba6",
+    "url": "/react-draggable-hooks/static/js/runtime~main.349843f2.js"
+  }
+]);
